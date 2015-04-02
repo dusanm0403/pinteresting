3768f45b2e8603888fbfa8ae7cfbb60e88b0244b
Dusan Marinic <dusanm0403@gmail.com>
2015-04-02 14:20:19 +0200

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
  
   def index
@@ -21,11 +22,10 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-    
-      if @pin.save
+    if @pin.save
        redirect_to @pin, notice: 'Pin was successfully created.'
       else
-       render :new
+       render action: 'new'
       end
   end
 
@@ -47,8 +47,13 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
     end
+
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pins
+  resources :pins 
 
   devise_for :users
   root "pages#home"
